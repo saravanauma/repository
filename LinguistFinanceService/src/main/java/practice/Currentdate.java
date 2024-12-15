771249289b06e6624d1c45aac296405b4a7c9f04
saravanauma <saravanauma26@gmail.com>
2024-12-15 07:02:39 +0530

    testing

--- LinguistFinanceService/src/main/java/practice/Currentdate.java
+++ LinguistFinanceService/src/main/java/practice/Currentdate.java
@@ -14,13 +14,14 @@ public class Currentdate {
 		date.toString();
 		System.out.println(date.toString());
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
 		
 		sdf.format(date);
 		System.out.println(sdf.format(date));
 		
 		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdfs = new SimpleDateFormat("MM/dd/yyyy hh:mm:SS");
+        cal.add(Calendar.DAY_OF_YEAR, -1);      // Add 1 day
+		SimpleDateFormat sdfs = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
         sdfs.format(cal.getTime());
 		System.out.println(sdfs.format(cal.getTime()));
 
